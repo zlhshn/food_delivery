dd5018593788019c19f7506abd5037b6f635f804
zlhshn <zsahin4401@gmail.com>
2024-10-03 09:59:06 +0300

    created file footer/appdownload

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExplorerMenu from "../../components/ExplorerMenu/ExplorerMenu";
 import FoodDisplay from "../../components/foodDisplay/FoodDisplay";
+import AppDownload from "../../components/AppDownload/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -12,6 +13,7 @@ const Home = () => {
       <Header />
       <ExplorerMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   );
 };
