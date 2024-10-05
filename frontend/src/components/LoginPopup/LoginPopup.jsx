bac7c9c542df066cd03ace95478bcc1ec28655e6
zlhshn <zsahin4401@gmail.com>
2024-10-05 16:19:21 +0300

    feat: Login card & Cart Page Design

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
 
-const LoginPopup = () => {
-  return <div>LoginPopup</div>;
+const LoginPopup = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Sign Up");
+
+  return (
+    <div className="login-popup">
+      <form action="" className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="your name" required />
+          )}
+
+          <input type="email" placeholder="your email" required />
+          <input type="password" placeholder="password" required />
+        </div>
+        <button>
+          {currentState === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrentState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default LoginPopup;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,33 +3,40 @@ import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("Home");
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to={"/"}>
+     
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
+
       <ul className="navbar-menu">
         <Link
-        to={"/"}
+          to={"/"}
           onClick={() => setMenu("Home")}
           className={menu === "Home" ? "active" : ""}
         >
           Home
         </Link>
-        <a href="#explore-menu"
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("Menu")}
           className={menu === "Menu" ? "active" : ""}
         >
           Menu
         </a>
-        <a href="#app-download"
+        <a
+          href="#app-download"
           onClick={() => setMenu("Mobile App")}
           className={menu === "Mobile App" ? "active" : ""}
         >
           Mobile App
         </a>
-        <a href="#footer"
+        <a
+          href="#footer"
           onClick={() => setMenu("Contact Us")}
           className={menu === "Contact Us" ? "active" : ""}
         >
@@ -39,10 +46,12 @@ const Navbar = () => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to={"/cart"}>
+            <img src={assets.basket_icon} alt="" />
+          </Link>
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
