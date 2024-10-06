e7d0c2b333fd0feb8fa6595e56cc8d8e0481bae2
zlhshn <zsahin4401@gmail.com>
2024-10-06 20:30:11 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 //app config
 
@@ -12,15 +13,20 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
-// db connection 
+// db connection
 
 connectDB();
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+// api endpoints
 
-app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port}`)
-})
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
