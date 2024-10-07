6f2925acc58218ef441d551fab1fcf27e1f67815
zlhshn <zsahin4401@gmail.com>
2024-10-07 23:30:35 +0300

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 //app config
 
@@ -21,7 +23,7 @@ connectDB();
 
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
-
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
