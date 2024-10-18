7d8a6dd9846ee32d880237582c6906116068ac38
Zeliha ŞAHİN <140537448+zlhshn@users.noreply.github.com>
2024-10-18 15:21:47 +0300

    verify

---
+++ frontend/src/pages/Verify/Verify.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Verify.css';
+import {useSearchParams} from 'react-router-dom';
+
+
+const Verify =()=>{
+
+const  [searchParams,setSearchParams] = useSearchParams()
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Verify;
