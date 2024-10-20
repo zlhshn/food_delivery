ed92e7ad24863077023be2a88cf1afffafe3e444
zlhshn <zsahin4401@gmail.com>
2024-10-20 16:23:40 +0300

    feat:verify page

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import './Verify.css';
-import {useSearchParams} from 'react-router-dom';
+import React, { useContext, useEffect } from "react";
+import "./Verify.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
+const Verify = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  // console.log(success,orderId);
+  const { url } = useContext(StoreContext);
+  const navigate = useNavigate();
 
-const Verify =()=>{
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/api/order/verify", {
+      success,
+      orderId,
+    });
+    if (response.data.success) {
+      navigate("/myorders");
+    } else {
+      navigate("/");
+    }
+  };
 
-const  [searchParams,setSearchParams] = useSearchParams()
+  useEffect(() => {
+    verifyPayment();
+  }, []);
 
-    return (
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <div className="verify">
+      <div className="spinner"></div>
+    </div>
+  );
+};
 
 export default Verify;
