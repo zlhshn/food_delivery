65b3212a24a66efcc453b0a9c8a4611043caece6
Zeliha ŞAHİN <140537448+zlhshn@users.noreply.github.com>
2024-10-22 11:05:51 +0300

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-t19e.onrender.com";
 
   return (
     <div>
